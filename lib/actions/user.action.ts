2523b3b448dfd9521d2890dc82de18e08fd147f4
Rohit Dewani <rohit.jd23@gmail.com>
2024-09-15 01:58:04 +0530

    fix: Fix delete user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -59,7 +59,7 @@ export async function deleteUser(params: DeleteUserParams) {
 
 		const { clerkId } = params;
 
-		const user = await User.findById({ clerkId });
+		const user = await User.findByIdAndDelete({ clerkId });
 
 		if (!user) {
 			throw new Error("User not found");
