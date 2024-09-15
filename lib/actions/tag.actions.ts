9a44b121d5d48f108a1e0111d383d3a106dbe958
Rohit Dewani <rohit.jd23@gmail.com>
2024-09-16 03:17:39 +0530

    feat: create tags page to display all tags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -4,7 +4,7 @@ import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 	try {
@@ -32,6 +32,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 		return tags;
 	} catch (error) {
 		console.log("🔴 Error getting user interacted tags", error);
+		throw error;
+	}
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+	try {
+		connectToDatabase();
+
+		const tags = await Tag.find({});
+
+		return { tags };
+	} catch (error) {
+		console.log("🔴 Error getting tags", error);
 		throw error;
 	}
 }
